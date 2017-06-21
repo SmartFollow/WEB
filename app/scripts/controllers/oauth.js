0bc639a5cc22bded9348999703726705e65d5603
François Kiene <francois.kiene@epitech.eu>
2017-06-21 11:20:46 +0200

    Change get user + add error in connection

--- app/scripts/controllers/oauth.js
+++ app/scripts/controllers/oauth.js
@@ -26,10 +26,19 @@ angular.module('oauthApp', ['routerApp', 'constants', 'user', 'ngCookies', 'angu
       scope: ""
     } 
     var data = OAuth.getAccessToken(user, options);
-    data.then(function(data) {
-        //console.log(data);
-        $window.location.reload();
-    });
+    data.then(function successCallback(data) {
+        //$window.location.reload();
+      users.getUserFromData(function (response) {
+          $rootScope.user = response;
+          if ($rootScope.user.id == 4)
+            $state.go('profil-student');
+          else
+            $state.go('profil-teacher');
+        })
+      }, function errorCallback(response) {
+          $("#error-auth .error").html(response.data.error);
+          $("#error-auth .message").html(response.data.message);
+      });
   }	
 }])
 
@@ -39,55 +48,3 @@ angular.module('oauthApp', ['routerApp', 'constants', 'user', 'ngCookies', 'angu
       $window.location.reload();
     }
 }]);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
-** Old connection
-*/
-
-/*
-angular.module('app', ['routerApp'])
-.controller('Connection', ['$scope', '$http', '$state', function($scope, $http, $state) {
-    //console.log(JSON.parse(window.localStorage.getItem("smartfollow")).oauth.access_token);
-    /*if (JSON.parse(window.localStorage.getItem("smartfollow")).oauth)
-      $state.go('connected');
-    $scope.submit = function () {
-        $http({
-           url: "http://smartfollow.api/oauth/token",
-           method: "POST",
-           data: {
-                username: $scope.inputEmail,
-                password: $scope.inputPassword,
-                grant_type: "password",
-                client_id: "2",
-                client_secret: "r2yiOu2YIKymJr0VBMxQC6QySCwniFMId8QqDefm",
-                scope: ""
-           }
-          }).then(function successCallback(response) {
-            var smartfollow = {
-                oauth: {
-                    access_token: response.access_token,
-                    expires_in: response.expires_in,
-                    refresh_token: response.refresh_token
-                }
-            };
-            // DOM Storage
-            window.localStorage.setItem("smartfollow", JSON.stringify(smartfollow));
-            $state.go('connected');
-          }, function errorCallback(response) {
-            alert("Le nom d'utilisateur ou le mot de passe n'est pas pas correct. Essayez à nouveau.");
-        });
-    }
-}]);*/
