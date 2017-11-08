196563bb91d47429825ec9be2a31611e3f5f1ba0
Antoine Buchser <antoine.buchser@epitech.eu>
2017-11-08 21:42:27 +0100

    Removed config.js from commited files

--- app/scripts/constants/config.js
+++
@@ -1,6 +0,0 @@
-angular.module('constants', [])
-	.constant('config', {
-		apiUrl: "http://api.dev.smartfollow.org/",
-		clientId: "2",
-		clientSecret: "fpWbyzAyOqQgugwnNZKIzxnbLuF5E5ZBK3Puf2IK"
-	});
