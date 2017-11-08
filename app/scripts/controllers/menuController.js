afdc681951a03b742163032c284737a45e8b7b2e
François Kiene <francois.kiene@epitech.eu>
2017-11-08 13:34:38 +0100

    add NotificationFactory method

--- app/scripts/controllers/menuController.js
+++ app/scripts/controllers/menuController.js
@@ -1,6 +1,6 @@
 angular.module('app')
 	.controller('MenuController', ['$rootScope', '$scope', '$state', 'config', 'NotificationFactory', function ($rootScope, $scope, $state, config, NotificationFactory) {
-		NotificationFactory.getNotifications(function (notifications) {
+		NotificationFactory.getSetNotifications(function (notifications) {
 			$scope.notifications = notifications;
 			$scope.notifications.forEach(notification => notification.updated_at = notification.updated_at ? new Date(notification.updated_at.replace('/-/g',"/")) : null);
 		});
--- app/scripts/factories/notificationFactory.js
+++ app/scripts/factories/notificationFactory.js
@@ -1,10 +1,62 @@
 angular.module('NotificationsModule')
 	.factory('NotificationFactory', ['$http', 'OAuth', 'config', function ($http, OAuth, config) {
 		return {
+			getSetNotifications: function (callback) {
+				$http({
+					method: 'GET',
+					url: config.apiUrl + "api/notifications"
+				}).then(function successCallback(response) {
+					callback(response.data);
+				}, function errorCallback(response) {
+					console.log(response);
+				});
+			},
 			getNotifications: function (callback) {
 				$http({
 					method: 'GET',
 					url: config.apiUrl + "api/notifications"
+				}).then(function successCallback(response) {
+					callback(response.data);
+				}, function errorCallback(response) {
+					console.log(response);
+				});
+			},
+			getCreateNotification: function (callback) {
+				$http({
+					method: 'GET',
+					url: config.apiUrl + "api/notifications/create"
+				}).then(function successCallback(response) {
+					callback(response.data);
+				}, function errorCallback(response) {
+					console.log(response);
+				});
+			},
+			storeNotification: function (data, callback) {
+				$http({
+					method: 'POST',
+					url: config.apiUrl + "api/notifications",
+					data: data
+				}).then(function successCallback(response) {
+					callback(response.data);
+				}, function errorCallback(response) {
+					console.log(response);
+				});
+			},
+			updateNotification: function (id, data, callback) {
+				$http({
+					method: 'PUT',
+					url: config.apiUrl + "api/notifications" + id,
+					data: data
+				}).then(function successCallback(response) {
+					callback(response.data);
+				}, function errorCallback(response) {
+					console.log(response);
+				});
+			},
+			deleteNotification: function (id, callback) {
+				$http({
+					method: 'DELETE',
+					url: config.apiUrl + "api/notifications/" + id
 				}).then(function successCallback(response) {
 					callback(response.data);
 				}, function errorCallback(response) {
