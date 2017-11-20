d60e9d26cacf027d1ad887dff6b2807ed06c2dac
Antoine Buchser <antoine.buchser@epitech.eu>
2017-11-20 19:58:17 +0100

    Removed unused files + added non-minified angular.js for development

--- app/scripts/constant/config.js
+++
@@ -1,6 +0,0 @@
-angular.module('constants', [])
-	.constant('config', {
-		apiUrl: "http://api.dev.smartfollow.lan/",
-		clientId: "2",
-		clientSecret: "SrAu1b95vln5Vgc7pW6pwvUYv9oHd2pfW74eIzuN"
-	});
