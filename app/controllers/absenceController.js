19fedb987e45655ea2f65ba0f060f44abd9521d2
Antoine Buchser <antoine.buchser@epitech.eu>
2017-12-21 22:23:57 +0100

    Indentation

--- app/controllers/absenceController.js
+++ app/controllers/absenceController.js
@@ -6,9 +6,9 @@ angular.module('AbsencesModule')
 			$scope.absences = absences;
 
 			$scope.absences.forEach(function (absence) {
-				absence.justified_at = absence.justified_at ? new Date(absence.justified_at.replace('/-/g',"/")) : null;
-				absence.evaluation.lesson.start = new Date(absence.evaluation.lesson.start.replace('/-/g',"/"));
-				absence.evaluation.lesson.end = new Date(absence.evaluation.lesson.end.replace('/-/g',"/"));
+				absence.justified_at = absence.justified_at ? new Date(absence.justified_at.replace('/-/g', "/")) : null;
+				absence.evaluation.lesson.start = new Date(absence.evaluation.lesson.start.replace('/-/g', "/"));
+				absence.evaluation.lesson.end = new Date(absence.evaluation.lesson.end.replace('/-/g', "/"));
 			});
 		});
 
@@ -19,9 +19,9 @@ angular.module('AbsencesModule')
 			AbsenceFactory.updateAbsence(absence.id, {
 				justified_at: justifiedAt
 			}, function (updatedAbsence) {
-				updatedAbsence.justified_at = new Date(updatedAbsence.justified_at.replace('/-/g',"/"));
-				updatedAbsence.evaluation.lesson.start = new Date(updatedAbsence.evaluation.lesson.start.replace('/-/g',"/"));
-				updatedAbsence.evaluation.lesson.end = new Date(updatedAbsence.evaluation.lesson.end.replace('/-/g',"/"));
+				updatedAbsence.justified_at = new Date(updatedAbsence.justified_at.replace('/-/g', "/"));
+				updatedAbsence.evaluation.lesson.start = new Date(updatedAbsence.evaluation.lesson.start.replace('/-/g', "/"));
+				updatedAbsence.evaluation.lesson.end = new Date(updatedAbsence.evaluation.lesson.end.replace('/-/g', "/"));
 
 				$scope.absences[$scope.absences.findIndex(e => e.id === updatedAbsence.id)] = updatedAbsence;
 			});
